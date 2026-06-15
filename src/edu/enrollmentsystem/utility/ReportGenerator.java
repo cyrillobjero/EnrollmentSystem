529be5450a0e09e5d5f87748b17b1d74f212628b
COLLEGELAB-08 <COLLEGELAB-08@BJSB.LAB>
2026-06-15 15:40:48 +0800

    addded code

--- src/edu/enrollmentsystem/utility/ReportGenerator.java
+++ src/edu/enrollmentsystem/utility/ReportGenerator.java
@@ -1,5 +1,20 @@
-package edu.module-collections.utility;
+package edu.cyrillmay.utility;
+
+import edu.cyrillmay.dto.EnrollmentDTO;
+import java.util.List;
 
 public class ReportGenerator {
-    
+
+    public void generateEnrollmentReport(
+            List<EnrollmentDTO> enrollments) {
+
+        for (EnrollmentDTO e : enrollments) {
+
+            System.out.println(
+                    e.getStudent().getName()
+                            + " -> "
+                            + e.getCourse().getCourseName()
+            );
+        }
+    }
 }
